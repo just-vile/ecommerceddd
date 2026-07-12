dc3c3186a5dab398a6201fc0c44bc55286f3d07c
just-vile <trieuvi27.le@gmail.com>
2026-07-12 19:58:52 +0700

    test: add mock for RuntimeConfigService in AppComponent tests

--- src/EcommerceDDD.Spa/src/app/app.component.spec.ts
+++ src/EcommerceDDD.Spa/src/app/app.component.spec.ts
@@ -2,18 +2,32 @@ import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { AppComponent } from './app.component';
-
+import { RuntimeConfigService } from './core/services/runtime-config.service';
+ 
+const runtimeConfigServiceMock = {
+  apiBaseUrl: 'http://localhost',
+  identityUrl: 'http://localhost/identity',
+  signalRHubUrl: 'http://localhost/signalr',
+  load: () => Promise.resolve(),
+};
+ 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [provideRouter([]), provideToastr()],
+      providers: [
+        provideRouter([]),
+        provideToastr(),
+        { provide: RuntimeConfigService, useValue: runtimeConfigServiceMock },
+      ],
     }).compileComponents();
   });
-
+ 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 });
+ 
+ 
